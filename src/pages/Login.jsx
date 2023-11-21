ec4c4443f060f81a8943fb604a12f19a2d3f6a8c
Dan G <daniel.w.gray1994@gmail.com>
2023-11-21 15:00:25 -0500

    added a form card with an email input field

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex justify-center my-5">
+      <div className="w-full max-w-xs">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-md font-bold mb-2">
+              Email
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Email address"
+            ></input>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
